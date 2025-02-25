5bd5a4ee0ed5c3f938bcd35fadb149e876210237
j0hanz <golfarenlj@gmail.com>
2025-02-25 21:16:02 +0100

    add Prettier configuration and update ESLint rules for consistency

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,9 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactCompiler from 'eslint-plugin-react-compiler'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactCompiler from 'eslint-plugin-react-compiler';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   { ignores: ['dist'] },
@@ -28,4 +28,4 @@ export default tseslint.config(
       'react-compiler/react-compiler': 'error',
     },
   },
-)
+);
