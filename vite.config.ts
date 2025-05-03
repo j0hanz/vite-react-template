b330717a2d365f755a332b7bfa027e73d02f8bf1
Linus Johansson <159924955+j0hanz@users.noreply.github.com>
2025-05-04 00:20:07 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,14 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    tsconfigPaths(),
     react({
       babel: {
         plugins: [['babel-plugin-react-compiler']],
       },
     }),
-    tsconfigPaths(),
   ],
 });
