5ed986fe117d1d56b936a662f53196e619247338
Linus Johansson <159924955+j0hanz@users.noreply.github.com>
2025-05-04 00:19:25 +0200

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,8 +4,9 @@ import reactCompiler from 'eslint-plugin-react-compiler';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
 import tseslint from 'typescript-eslint';
-import eslintConfigPrettier from 'eslint-config-prettier';
-import react from 'eslint-plugin-react';
+import simpleImportSortPlugin from 'eslint-plugin-simple-import-sort';
+import reactPlugin from 'eslint-plugin-react';
+import prettierConfig from 'eslint-config-prettier';
 
 export default tseslint.config(
   { ignores: ['dist'] },
@@ -13,61 +14,45 @@ export default tseslint.config(
     extends: [
       js.configs.recommended,
       ...tseslint.configs.recommended,
-      eslintConfigPrettier,
+      ...tseslint.configs.recommendedTypeChecked,
+      ...tseslint.configs.strict,
+      ...tseslint.configs.strictTypeChecked,
+      prettierConfig,
     ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2022,
       globals: globals.browser,
       parserOptions: {
+        project: ['./tsconfig.app.json', './tsconfig.node.json'],
+        tsconfigRootDir: '.',
+        sourceType: 'module',
         ecmaFeatures: {
           jsx: true,
         },
-        project: ['./tsconfig.node.json', './tsconfig.app.json'],
-        tsconfigRootDir: '.',
       },
     },
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
       'react-compiler': reactCompiler,
-      react,
-    },
-    settings: {
-      react: {
-        version: '19',
-        fragment: 'Fragment',
-      },
-      linkComponents: [{ name: 'Link', linkAttribute: 'to' }],
+      'simple-import-sort': simpleImportSortPlugin,
+      react: reactPlugin,
     },
     rules: {
-      ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
-      'react/jsx-key': 'error',
-      'react/jsx-no-comment-textnodes': 'error',
-      'react/jsx-no-duplicate-props': 'error',
-      'react/jsx-no-target-blank': ['error', { enforceDynamicLinks: 'always' }],
-      'react/jsx-no-undef': 'error',
-      'react/jsx-uses-vars': 'error',
-      'react/no-children-prop': 'error',
-      'react/no-danger-with-children': 'error',
-      'react/no-direct-mutation-state': 'error',
-      'react/no-unescaped-entities': 'error',
-      'react/no-unknown-property': ['error', { ignore: ['css'] }],
-      'react/require-render-return': 'error',
-      'react/self-closing-comp': ['error', { component: true, html: true }],
-      'react/jsx-curly-brace-presence': [
-        'error',
-        { props: 'never', children: 'never' },
-      ],
-      'react/jsx-fragments': ['error', 'syntax'],
-      'react/no-unstable-nested-components': 'warn',
-      'react/prop-types': 'off',
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
       ],
       'react-compiler/react-compiler': 'error',
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
     },
   },
 );
