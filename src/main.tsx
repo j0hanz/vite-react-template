fea06c8dbe4e5b8c5c8164ec537ad81697dac44d
j0hanz <golfarenlj@gmail.com>
2025-05-20 19:52:34 +0200

    chore: replace Fredoka font with Roboto and update related imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import '@fontsource/roboto/400.css';
 import './index.css';
 import App from '@/App';
 import { ColorModeProvider } from '@/components/ColorTheme';
